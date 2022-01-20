63b143e4b22e05e5840b12776db20d9e648f32ac
Vladimir <80447144+VovShevtsov@users.noreply.github.com>
2022-01-20 13:21:30 +0300

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log('global');
+/**
+  * название функции
+  *
+  * @param {number} first - первое число
+  * @returns {number}
+  */
+"use strict";
+//# sourceMappingURL=main.js.map
